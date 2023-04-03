742949a3a1940ecc71b888e130f0ef26a27b61e9
Michael Berkowitz <michael.berkowitz@gmail.com>
2023-04-02 22:53:42 -0400

    Add inversion functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub type Set = Vec<u32>;
 
+mod inversion;
 mod normal_form;
 mod transposition;
 
@@ -13,16 +14,53 @@ pub fn transpose(set: &Set, level: u32) -> Set {
     transposition::by(set, level)
 }
 
+pub fn invert(set: &Set, level: u32) -> Set {
+    inversion::by(set, level)
+}
+
+pub fn invert_by_pair(set: &Set, inversion_pair: (u32, u32)) -> Set {
+    inversion::by_pair(set, inversion_pair)
+}
+
 define_transpositions!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+define_inversions!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
 
 #[cfg(test)]
 mod tests {
-    use crate::{t3, Set};
+    use crate::{i5, invert, invert_by_pair, t3, transpose, Set};
+
+    #[test]
+    fn transposition() {
+        let set: Set = vec![7, 8, 10, 11];
+
+        assert_eq!(transpose(&set, 3), vec![10, 11, 1, 2]);
+    }
 
     #[test]
     fn generated_transposition_levels() {
         let set: Set = vec![7, 8, 10, 11];
 
         assert_eq!(t3(&set), vec![10, 11, 1, 2]);
+    }
+
+    #[test]
+    fn inversion() {
+        let set: Set = vec![1, 3, 4, 7];
+
+        assert_eq!(invert(&set, 5), vec![10, 1, 2, 4]);
+    }
+
+    #[test]
+    fn inversion_by_pair() {
+        let set: Set = vec![7, 8, 11];
+
+        assert_eq!(invert_by_pair(&set, (7, 11)), vec![7, 10, 11]);
+    }
+
+    #[test]
+    fn generated_inversion_levels() {
+        let set: Set = vec![1, 3, 4, 7];
+
+        assert_eq!(i5(&set), vec![10, 1, 2, 4]);
     }
 }
